2beb260ce000df2eb75ce67e8a087ce0a25c4f0a
Patrick Felixberger <robomaniac8@googlemail.com>
2024-03-07 00:27:36 +0100

    Fixed config path for appimage
    Signed-off-by: Patrick Felixberger <robomaniac8@googlemail.com>

--- src/utils/profiles.cpp
+++ src/utils/profiles.cpp
@@ -4,17 +4,27 @@
 // chosen for backward compatibility with pre-profiles versions, which always stored settings in `settings.ini`
 const QString default_profile_name = "settings";
 
-QStringList getProfileNames() {
+QStringList getProfileNames()
+{
     const QDir configDir = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
     const QStringList configFiles = configDir.entryList({"*.ini"}, QDir::Files);
     QStringList rv;
-    for (QString configFile : configFiles) {
+    for (QString configFile : configFiles)
+    {
         rv << QUrl::fromPercentEncoding(configFile.replace(QRegExp("\\.ini$"), "").toUtf8());
     }
     return rv;
 }
 
-QString configPathForProfile(QString profile_name) {
-    const QDir configDir = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
+QString configPathForProfile(QString profile_name)
+{
+    std::string configPath = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation).toStdString();
+    auto pos = configPath.rfind('/');
+    if (pos != std::string::npos)
+    {
+        configPath.erase(pos);
+    }
+    QString tmp = QString::fromStdString(configPath) + "/Candle2";
+    QDir configDir(tmp);
     return configDir.filePath(QUrl::toPercentEncoding(profile_name) + ".ini");
 }
